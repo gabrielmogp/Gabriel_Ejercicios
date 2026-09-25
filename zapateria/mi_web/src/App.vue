f8be9b3f602474edf7fc70a302f31f06f89ebd46
gabrielmogp <gabrielmogp@gmail.com>
2026-09-25 13:18:39 +0200

    zapateria: Implementacion de componente Catalogo y actualizacion de componente App, cabezera, catalogo, inicio

--- zapateria/mi_web/src/App.vue
+++ zapateria/mi_web/src/App.vue
@@ -1,12 +1,22 @@
 <script setup>
+import {ref} from 'vue'
 import Cabecera from './components/Cabecera.vue'
 import Inicio from './components/Inicio.vue'
 import Modelos from './components/Modelos.vue'
 import Pie from './components/Pie.vue'
+import Catalog from './components/catalogo.vue'
+import Catalogo from './components/catalogo.vue'
+
+const mostrarCatalogo = ref(false)
 </script>
 
 <template>
-  <Cabecera />
-  <main><Inicio /><Modelos /></main>
+  <Cabecera @abrir-modelos="mostrarCatalogo=true"/>
+  <main>
+    <Inicio @abrir-modelos="mostrarCatalogo=true" />
+    <Modelos />
+  </main>
   <Pie />
+
+  <Catalogo :visible="mostrarCatalogo" @cerrar="mostrarCatalogo = false"/>
 </template>
